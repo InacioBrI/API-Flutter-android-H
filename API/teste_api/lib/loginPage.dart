98fad27ab99071b0b2c6493f4bc7d142143ce462
Inacio Barboza De Lima <InacioBrwI@hotmail.com>
2023-05-30 21:26:02 -0300

    Colocando UrI parse para usuario e senha.

--- API/teste_api/lib/loginPage.dart
+++ API/teste_api/lib/loginPage.dart
@@ -17,58 +17,68 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: _formkey,
-        child: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'email'
+      return Scaffold(
+        body: Form(
+          key: _formkey,
+          child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'email'
+                  ),
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (email){
+                    if(email == null || email.isEmpty) {
+                      return 'Por favor, digite seu email';
+                    }else if  (!RegExp(
+                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                      .hasMatch(_emailController.text)) {
+                      return 'Por favor, digite um e-mail correto';
+                    }
+                    return null;
+                  },
                 ),
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                validator: (email){
-                  if(email == null || email.isEmpty) {
-                    return 'Por favor, digite seu email';
-                  }else if  (!RegExp(
-                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                    .hasMatch(_emailController.text)) {
-                    return 'Por favor, digite um e-mail correto';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                labelText: 'senha'
+                TextFormField(
+                  decoration: const InputDecoration(
+                  labelText: 'senha'
+                  ),
+                  controller: _passwordController,
+                  keyboardType: TextInputType.text,
+                  validator: (senha){
+                    if(senha == null || senha.isEmpty){
+                      return 'Por favor, digite sua senha';
+                    }else if(senha.length  < 6){
+                      return 'Por favor, digite uma senha maior 6 caracteres';
+                    }
+                    return null;
+                  },
                 ),
-                controller: _passwordController,
-                keyboardType: TextInputType.text,
-                validator: (senha){
-                  if(senha == null || senha.isEmpty){
-                    return 'Por favor, digite sua senha';
-                  }else if(senha.length  < 6){
-                    return 'Por favor, digite uma senha maior 6 caracteres';
-                  }
-                  return null;
-                },
-              ),
-              ElevatedButton(onPressed: (){}, child: const Text('ENTRAR'),),
-            ],
+                ElevatedButton(
+                  onPressed: (){},
+                  child: const Text('ENTRAR'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ),
     );
   }
-}
 
-Future<bool> login() async{
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  var url = Uri.parse('https//minhasapis.com.br/login/');
+
+  Future<bool> login() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    var url = Uri.parse('https//minhasapis.com.br/login/');
+    var resposta = await http.post(url,
+      body: {
+        'usarname': _emailController.text
+        'password': _passwordController.text
+      },
+    );
+  }
 }
